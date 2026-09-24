09ba9965f6c11389b1bd5508f9f305a670d2fb19
David Edwards <davidedwards@gmail.com>
2026-09-24 16:49:49 -0700

    update showtimes

--- liveklos.sh
+++ liveklos.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
-# cd /mnt/c/Users/daedw/HAF
-
+# 1. Generate the unique daily filename
 c=1
-#echo $file
 file=$(date +"%Y_%m_%d_0"$c)
 
-while test -e $file; do 
-    echo $file' exists'
-    NUM=$[$NUM + 1]
-    c=$[$c+1]
+while test -e "$file"; do 
+    echo "$file exists"
+    c=$((c + 1))
     file=$(date +"%Y_%m_%d_0"$c)
 done
-echo "creating new: "$file
+echo "Creating new file: $file"
 
-# https://pwaimg.listenlive.co/KLOSFM_2050411
-# curl --ssl-no-revoke https://23113.live.streamtheworld.com/KLOSFMAAC.aac -o $file
-# curl --insecure --ssl-no-revoke https://24883.live.streamtheworld.com/KLOSFMAAC.aac -o $file
-# curl --insecure --ssl-no-revoke https://23113.live.streamtheworld.com/KLOSFMAAC.aac -o $file
-# curl --insecure --ssl-no-revoke https://22973.live.streamtheworld.com/KLOSFMAAC_SBM -o $file
-curl --insecure --ssl-no-revoke https://22973.live.streamtheworld.com/KLOSFMAAC.aac -o $file
-# curl --insecure --ssl-no-revoke https://27163.live.streamtheworld.com/KLOSFMAAC_SBM -o $file
-# curl --insecure --ssl-no-revoke https://27163.live.streamtheworld.com/KLOSFMAAC.aac -o $file
-# curl --ssl-no-revoke https://25053.live.streamtheworld.com/KLOSFMAAC_SBM -o $file
-# curl --ssl-no-revoke http://25013.live.streamtheworld.com/KLOSHD2AAC -o $file
-# curl --ssl-no-revoke http://22983.live.streamtheworld.com/KLOSHD2AAC -o $file
-# curl --ssl-no-revoke http://16603.live.streamtheworld.com/KLOSHD2AAC -o $file
-# curl --ssl-no-revoke https://16603.live.streamtheworld.com/KLOSHD2AAC_SBM -o $file
-# curl --ssl-no-revoke -k --insecure https://16603.live.streamtheworld.com/KLOSHD2AAC_SBM -o $file
-# curl --ssl-no-revoke https://16603.live.streamtheworld.com/KLOSHD2AAC -o $file
-# curl --ssl-no-revoke https://22973.live.streamtheworld.com/KLOSHD2AAC -o $file
-# curl -k --insecure --ssl-no-revoke https://25013.live.streamtheworld.com/KLOSFMAAC -o $file
-# echo $(date +"%Y_%m_%d_0").aac
+# 2. Wait until exactly 6:10 AM PST/PDT
+# (Calculates the gap between current time and target time in seconds)
+TARGET_TIME="06:10:00"
+CURRENT_TIME=$(date +"%H:%M:%S")
+
+# Convert both times to seconds since midnight to calculate the sleep duration
+target_sec=$(date -d "$TARGET_TIME" +%s)
+current_sec=$(date -d "$CURRENT_TIME" +%s)
+sleep_duration=$((target_sec - current_sec))
+
+if [ $sleep_duration -gt 0 ]; then
+    echo "Current time is $CURRENT_TIME. Waiting $sleep_duration seconds until $TARGET_TIME..."
+    sleep $sleep_duration
+fi
+
+echo "It is now 6:10 AM. Starting recording until 10:00 AM..."
+
+# 3. Record for exactly 3 hours and 50 minutes (13,800 seconds)
+STREAM_URL="https://22973.live.streamtheworld.com/KLOSFMAAC.aac"
+curl --insecure --ssl-no-revoke --max-time 13800 "$STREAM_URL" -o "$file"
+
+echo "Recording finished at 10:00 AM."
